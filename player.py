997eb25b4fe1bd668d13cf094652476022521104
Mario Ehiagwina0027 <mario.ehiagwina0027@stud.hkr.se>
2025-10-22 12:18:49 +0200

    doctrings on player

--- player.py
+++ player.py
@@ -4,11 +4,19 @@ class Player:
         self.total_score = 0
         self.current_score = 0
 
+        
+
     def add_total_score(self, points):
+        """""This function returns the totalscore of the
+        currentuser """
         self.total_score += points
         return self.total_score
     
     def change_name(self, name_dict):
+        """""This function chnages name of the current user by asking
+        for the current name to change and changing it to the new name typed
+        names are stored in the dictornary, current name gets removed and
+        replace with the new one"""
         current_name = input('Enter current name: ')
         new_name = input('Enter new name: ')
 
@@ -21,5 +29,7 @@ class Player:
             print(f'{current_name} not found')
 
     def reset_turn(self):
+        """""This functions resets a drag, if the currentscore is 15
+        it resets to 0"""
         self.current_score = 0
         print(f'{self.name}s turn has been reset. Current score is now {self.current_score}')
